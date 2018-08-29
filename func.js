78eba5543500892b20c2e756755ada4f88cb8a73
Ronny Håland <ronnyhaland@gmail.com>
2018-08-29 23:27:09 +0200

    moved most function to func.js, added coloring on portals and started on a selection tool

---
+++ func.js
@@ -0,0 +1,199 @@
+function getMousePos(canvas, e) {
+    var rect = canvas.getBoundingClientRect();
+    return {
+        x: e.clientX - rect.left,
+        y: e.clientY - rect.top
+    };
+}
+
+function createWall(x,y) {
+    if (level.walls.filter(e => e.x == x && e.y == y).length > 0){
+        return
+    } else {
+        remove(x,y);
+        level.walls.push({'x':x,'y':y});
+        drawAll();
+    }
+}
+
+function createPlayer(x,y) {
+    remove(x,y);
+    level.player = {'x':x,'y':y};
+    drawAll();
+}
+
+function createGoal(x,y) {
+    remove(x,y);
+    level.goal = {'x':x,'y':y};
+    drawAll();
+}
+
+function createTp(x,y) {
+    remove(x,y);
+    level.specials.push({'x':x,'y':y,'type':'teleporter',id:Math.floor(level.specials.length/2)});
+    drawAll();
+}
+
+function remove(x,y) {
+    var foundWalls = level.walls.filter(e => e.x == x && e.y == y)
+    var foundSpecials = level.specials.filter(e => e.x-16 == x && e.y-16 == y)
+    if (foundWalls.length > 0){
+        foundWalls.forEach(function(e){
+            var ind = level.walls.indexOf(e);
+            level.walls.splice(ind,1);
+            drawAll();
+        })
+    }
+    if (foundSpecials.length > 0){
+        foundSpecials.forEach(function(e){
+            var ind = level.specials.indexOf(e);
+            level.specials.splice(ind,1);
+            drawAll();
+        })
+    }
+    if (level.player.x-16 == x && level.player.y-16 == y){
+        level.player = {};
+    }
+    if (ibetween(x,level.goal.x-47,level.goal.x+31) && ibetween(y,level.goal.y-47,level.goal.y+31)){
+        level.goal = {};
+    }
+}
+
+// check if p is between min and max
+function between(p, min, max) {
+    if (p>min && p<max) {return true}
+    else return false
+}
+
+//check if p is between or equal to min and/or max
+function ibetween(p, min, max) {
+    if (p>=min && p<=max) {return true}
+    else return false
+}
+
+function updateName() {
+    level.name = document.getElementById("levelName").value
+}
+
+function updateStars(id) {
+    level.req[id] = Number(document.getElementById("level"+id+"star").value);
+}
+
+function drawCursorObject() {
+    drawAll();
+    context.fillStyle = '#aaa';
+    context.globalAlpha = 0.5;
+
+    if (mouseRightPressed) {
+        remove(gridPos.x,gridPos.y);
+        context.fillStyle = '#F66'
+        context.fillRect(gridPos.x, gridPos.y,grid.x,grid.y);
+    } else if (placeItem.value === "wall"){
+        context.fillRect(gridPos.x, gridPos.y,grid.x,grid.y);
+        if (mouseLeftPressed){
+            createWall(gridPos.x,gridPos.y);
+        }
+    } else if (placeItem.value === "player") {
+        context.beginPath();
+        context.arc(gridPos.x+16,gridPos.y+16,8,0,2*Math.PI);
+        context.fillStyle = "#5aF"
+        context.fill();
+    } else if (placeItem.value === "goal") {
+        context.beginPath();
+        context.arc(Math.max(gridPos.x,32),Math.max(gridPos.y,32),32,0,2*Math.PI);
+        context.fillStyle = "#5aF"
+        context.fill();
+    } else if (placeItem.value === "tp") {
+        context.beginPath();
+        context.arc(gridPos.x+16,gridPos.y+16,16,0,2*Math.PI);
+        context.fillStyle = "#F55"
+        context.fill();
+    } else if (placeItem.value === "select") {
+        var x1,x2,y1,y2,w,h,bx,by;
+        if (!mouseLeftPressed) {
+            x1=gridPos.x;x2=gridPos.x+grid.x;
+            y1=gridPos.y;y2=gridPos.y+grid.y;
+            h=grid.y;w=grid.x;
+        } else {
+            bx = 0; by = 0;
+            if (gridPos.x<selectStart.x){bx=grid.x}
+            if (gridPos.y<selectStart.y){by=grid.y}
+            x1=selectStart.x+bx;x2=gridPos.x+grid.x-bx;
+            y1=selectStart.y+by;y2=gridPos.y+grid.y-by;
+            h=gridPos.y+grid.y-selectStart.y-2*by*(gridPos.y<selectStart.y);
+            w=gridPos.x+grid.x-selectStart.x-2*bx*(gridPos.x<selectStart.x);
+
+        }
+        context.strokeStyle = "#00F"
+        context.fillStyle = "#55c"
+        context.globalAlpha = 0.2;
+        context.fillRect(x1, y1, w, h);
+        context.globalAlpha = 0.6;
+        context.beginPath();
+        context.moveTo(x1,y1);
+        context.lineTo(x1,y2);
+        context.lineTo(x2,y2);
+        context.lineTo(x2,y1);
+        context.lineTo(x1,y1);
+        context.stroke();
+    }
+    context.globalAlpha = 1;
+
+}
+
+function drawGrid() {
+    context.globalAlpha = .35;
+    for (var i = 0; i<canvas.height; i+=grid.y){
+        x1=0;x2=canvas.width;
+        y1=i;y2=i;
+        context.strokeStyle = "#000";
+        context.beginPath();
+        context.moveTo(x1,y1);
+        context.lineTo(x2,y2);
+        context.stroke();
+    }
+    for (var i = 0; i<canvas.width; i+=grid.x){
+        y1=0;y2=canvas.height;
+        x1=i;x2=i;
+        context.strokeStyle = "#000";
+        context.beginPath();
+        context.moveTo(x1,y1);
+        context.lineTo(x2,y2);
+        context.stroke();
+    }
+    context.globalAlpha = 1;
+}
+
+function drawAll() {
+    context.globalAlpha = 1;
+    context.clearRect(0, 0, canvas.width, canvas.height);
+    drawGrid();
+    context.fillStyle='#aaa';
+    level.walls.forEach(function(e) {
+        context.fillRect(e.x,e.y,grid.x,grid.y);
+    });
+    if ('x' in level.player && 'y' in level.player) {
+        context.beginPath();
+        context.arc(level.player.x,level.player.y,8,0,2*Math.PI);
+        context.fillStyle = "#5A5"
+        context.fill();
+    }
+    if ('x' in level.goal && 'y' in level.goal) {
+        context.beginPath();
+        context.arc(level.goal.x,level.goal.y,32,0,2*Math.PI);
+        context.fillStyle = "#5AF"
+        context.fill();
+    }
+    level.specials.forEach(function(e){
+        if (e.type == 'teleporter'){
+            context.beginPath();
+            context.arc(e.x,e.y,16,0,2*Math.PI);
+            var r = Math.floor(2+e.id*2.33*Math.PI)%16;
+            var g = Math.floor(3+e.id*Math.PI*Math.PI)%16;
+            var b = Math.floor(4+e.id*13.88945*Math.PI)%16;
+            context.fillStyle = "#"+r.toString(16)+g.toString(16)+b.toString(16)
+            context.fill();
+        }
+    });
+
+}
--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const grid = {x:32,y:32};
 var mousePos, gridPos;
 var level = {'walls':[],'specials':[],'player':{},'goal':{},'name':'',"req":{"1":4000,"2":3000,"3":2000}};
+var selectStart = {x:0,y:0};
 var context, canvas, placeItem, mouseLeftPressed, mouseRightPressed;
 
 window.onload = function() {
@@ -31,6 +32,8 @@ window.onload = function() {
                 createGoal(gridPos.x,gridPos.y);
             } else if (placeItem.value === "tp"){
                 createTp(gridPos.x+16,gridPos.y+16);
+            } else if (placeItem.value === "select"){
+                selectStart = {x: gridPos.x, y:gridPos.y};
             }
         }
         else if (e.button == 2){
@@ -72,173 +75,6 @@ window.onload = function() {
         if (key <= '9' && key >= '0'){
             console.log(123)
             drawCursorObject();
-        }
-    });
-}
-
-function getMousePos(canvas, e) {
-    var rect = canvas.getBoundingClientRect();
-    return {
-        x: e.clientX - rect.left,
-        y: e.clientY - rect.top
-    };
-}
-
-function createWall(x,y) {
-    if (level.walls.filter(e => e.x == x && e.y == y).length > 0){
-        return
-    } else {
-        remove(x,y);
-        level.walls.push({'x':x,'y':y});
-        drawAll();
-    }
-}
-
-function createPlayer(x,y) {
-    remove(x,y);
-    level.player = {'x':x,'y':y};
-    drawAll();
-}
-
-function createGoal(x,y) {
-    remove(x,y);
-    level.goal = {'x':x,'y':y};
-    drawAll();
-}
-
-function createTp(x,y) {
-    remove(x,y);
-    level.specials.push({'x':x,'y':y,'type':'teleporter'});
-    drawAll();
-}
-
-function remove(x,y) {
-    var foundWalls = level.walls.filter(e => e.x == x && e.y == y)
-    var foundSpecials = level.specials.filter(e => e.x-16 == x && e.y-16 == y)
-    if (foundWalls.length > 0){
-        foundWalls.forEach(function(e){
-            var ind = level.walls.indexOf(e);
-            level.walls.splice(ind,1);
-            drawAll();
-        })
-    }
-    if (foundSpecials.length > 0){
-        foundSpecials.forEach(function(e){
-            var ind = level.specials.indexOf(e);
-            level.specials.splice(ind,1);
-            drawAll();
-        })
-    }
-    if (level.player.x == x && level.player.y == y){
-        level.player = {};
-    }
-    if (ibetween(x,level.goal.x-47,level.goal.x+31) && ibetween(y,level.goal.y-47,level.goal.y+31)){
-        level.goal = {};
-    }
-}
-// check if p is between min and max
-function between(p, min, max) {
-    if (p>min && p<max) {return true}
-    else return false
-}
-
-//check if p is between or equal to min and/or max
-function ibetween(p, min, max) {
-    if (p>=min && p<=max) {return true}
-    else return false
-}
-
-function updateName() {
-    level.name = document.getElementById("levelName").value
-}
-
-function updateStars(id) {
-    level.req[id] = Number(document.getElementById("level"+id+"star").value);
-}
-
-function drawCursorObject() {
-    drawAll();
-    context.fillStyle = '#aaa';
-    context.globalAlpha = 0.5;
-
-    if (mouseRightPressed) {
-        remove(gridPos.x,gridPos.y);
-        context.fillStyle = '#F66'
-        context.fillRect(gridPos.x, gridPos.y,grid.x,grid.y);
-    } else if (placeItem.value === "wall"){
-        context.fillRect(gridPos.x, gridPos.y,grid.x,grid.y);
-        if (mouseLeftPressed){
-            createWall(gridPos.x,gridPos.y);
-        }
-    } else if (placeItem.value === "player") {
-        context.beginPath();
-        context.arc(gridPos.x+16,gridPos.y+16,8,0,2*Math.PI);
-        context.fillStyle = "#5aF"
-        context.fill();
-    } else if (placeItem.value === "goal") {
-        context.beginPath();
-        context.arc(Math.max(gridPos.x,32),Math.max(gridPos.y,32),32,0,2*Math.PI);
-        context.fillStyle = "#5aF"
-        context.fill();
-    } else if (placeItem.value === "tp") {
-        context.beginPath();
-        context.arc(gridPos.x+16,gridPos.y+16,16,0,2*Math.PI);
-        context.fillStyle = "#F55"
-        context.fill();
-    }
-    context.globalAlpha = 1;
-
-}
-
-function drawGrid() {
-    context.globalAlpha = .35;
-    for (var i = 0; i<canvas.height; i+=grid.y){
-        x1=0;x2=canvas.width;
-        y1=i;y2=i;
-        context.strokeStyle = "#000";
-        context.beginPath();
-        context.moveTo(x1,y1);
-        context.lineTo(x2,y2);
-        context.stroke();
-    }
-    for (var i = 0; i<canvas.width; i+=grid.x){
-        y1=0;y2=canvas.height;
-        x1=i;x2=i;
-        context.strokeStyle = "#000";
-        context.beginPath();
-        context.moveTo(x1,y1);
-        context.lineTo(x2,y2);
-        context.stroke();
-    }
-    context.globalAlpha = 1;
-}
-
-function drawAll() {
-    context.globalAlpha = 1;
-    context.clearRect(0, 0, canvas.width, canvas.height);
-    drawGrid();
-    context.fillStyle='#aaa';
-    level.walls.forEach(function(e) {
-        context.fillRect(e.x,e.y,grid.x,grid.y);
-    });
-    if ('x' in level.player && 'y' in level.player) {
-        context.beginPath();
-        context.arc(level.player.x,level.player.y,8,0,2*Math.PI);
-        context.fillStyle = "#5A5"
-        context.fill();
-    }
-    if ('x' in level.goal && 'y' in level.goal) {
-        context.beginPath();
-        context.arc(level.goal.x,level.goal.y,32,0,2*Math.PI);
-        context.fillStyle = "#5AF"
-        context.fill();
-    }
-    level.specials.forEach(function(e){
-        if (e.type == 'teleporter'){
-            context.beginPath();
-            context.arc(e.x,e.y,16,0,2*Math.PI);
-            context.fillStyle = "#A55"
-            context.fill();
         }
     });
 
